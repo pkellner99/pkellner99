347ca9beb0bf9e43eb2a92dfcb5217c4c64de2c7
pkellner <peter@peterkellner.net>
2024-02-24 10:29:59 -0800

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
