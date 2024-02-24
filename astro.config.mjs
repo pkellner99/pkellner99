295240d26f5742f51c9b80d6cc3e6844db4a4538
pkellner <peter@peterkellner.net>
2024-02-24 09:52:25 -0800

    go

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
+});
--- src/utils/astro-jekyll.d.ts
+++
@@ -1,9 +0,0 @@
-// ./astro-jekyll.d.ts
-export interface JekyllCollectionEntry {
-  slug: string;
-  data: {
-    date: Date;
-    permalink?: string;
-    [key: string]: unknown; // Allows for additional properties as needed
-  };
-}
