71a2b9f40823b6b7baab254c93c2817752972116
pkellner <peter@peterkellner.net>
2024-02-24 07:51:59 -0800

    goG

--- src/components/CourseCarousel.tsx
+++ src/components/CourseCarousel.tsx
@@ -64,7 +64,7 @@ export default function CourseCarousel() {
           width: `${courses.length * 100}%`,
           transform: `translateX(-${(activeIndex * 100) / courses.length}%)`,
         }}
-      >
+      >XXX
         {courses.map((course, index) => {
           const str = `https://${course.link}`;
           return (
