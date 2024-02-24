396070909ce4e6acd3fa963e4a41b6eca8ff0703
pkellner <peter@peterkellner.net>
2024-02-24 10:45:52 -0800

    gogogo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-import node from "@astrojs/node";
 
 import vercel from "@astrojs/vercel/serverless";
 
