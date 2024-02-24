33385547909683be2f90d4d371e2f72e2cb36ef0
pkellner <peter@peterkellner.net>
2024-02-24 10:55:39 -0800

    go

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,8 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: 'pkellner99/pkellner99'
   },
   collections: {
     posts: collection({
